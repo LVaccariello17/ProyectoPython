a2965f8697d54abb3bf471a9e7960650f5fce82d
LVaccariello17 <55666064+LVaccariello17@users.noreply.github.com>
2021-10-30 18:19:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
-from smtplib import *
-
-import tkinter
-
 import mysql.connector
 
 cnx = mysql.connector.connect(user='root', password='lucas123',
@@ -20,15 +16,15 @@ class Login:
         self.wind = window
         self.wind.geometry("400x400")
         self.wind.title('Registro e inicio de sesión')
-        
+       
 
         frame = LabelFrame(self.wind, text = 'Inicio de sesión', height = 200)
         frame.grid(row = 0, column = 0, columnspan = 3, pady = 50, padx = 50)
-        
-        
+       
+       
         userverify = StringVar()
         passwordverify = StringVar()
-        
+       
         # Usuario
         Label(frame, text = 'Usuario: ').grid(row = 1, column = 0, pady = 10)
         self.user = Entry(frame, textvariable = userverify)
@@ -37,7 +33,7 @@ class Login:
 
         # Contraseña
         Label(frame, text = 'Contraseña: ').grid(row = 2, column = 0, pady = 5)
-        self.password = Entry(frame, textvariable = passwordverify, show = '*')
+        self.password = Entry(frame, show ='*', textvariable = passwordverify)
         self.password.focus()
         self.password.grid(row = 2, column = 1)
 
@@ -45,23 +41,23 @@ class Login:
         ttk.Button(frame, text = 'Iniciar sesión', command = lambda: self.iniciar_sesion(userverify, passwordverify)).grid(row = 3, columnspan = 2, sticky = W + E, pady = 5)
         ttk.Button(frame, text = 'Crear cuenta', command = self.crear_cuenta).grid(row = 4, columnspan = 2, sticky = W + E, pady = 5)
         ttk.Button(frame, text = 'Recuperar contraseña', command = self.recuperar_contrasenia).grid(row = 5, columnspan = 2, sticky = W + E, pady = 5)
-        
+       
 
 
     # Recuperar contraseña
     def recuperar_contrasenia (self):
-        
+       
         messagebox.showinfo(title="Contraseña", message="Se le ha enviado su contraseña por email")
 
     # Creación cuenta
-    def crear_cuenta (self): 
+    def crear_cuenta (self):
         self.crearcuenta = Toplevel()
-        self.crearcuenta.title = 'Editar producto'
+        self.crearcuenta.title = 'Registro'
         self.crearcuenta.geometry("400x400")
-        
+       
         header = Label(self.crearcuenta, text = "Pantalla de registro")
         header.grid(row = 0, column = 0 , columnspan = 4)
-        
+       
         frame = LabelFrame(self.crearcuenta, height = 500)
         frame.grid(row = 0, column = 0, columnspan = 4, pady = 50, padx = 50, sticky = N + S)
 
@@ -94,17 +90,17 @@ class Login:
     def validacion(self):
         if '@' and '.com' in self.mail.get():
             return len(self.user.get()) != 0 and len(self.password.get()) != 0
-        
+       
         else:
             messagebox.showinfo(message="El email ingresado no es correcto")
-        
+       
    
     def insertar_usuarios(self):
         if self.validacion():
-            query = """INSERT INTO usuarios (iduser, nombreuser, mail, contraseña) VALUES (NULL,%s,%s,%s)"""
+            query = """INSERT INTO usuarios (iduser, nombreuser, mail, contrasena) VALUES (NULL,%s,%s,%s)"""
             parametros =  (self.user.get(), self.mail.get(), self.password.get())
             self.consultar(query, parametros)
-            messagebox.showinfo(message="Registro exitoso")           
+            messagebox.showinfo(message="Registro exitoso")          
             self.user.delete(0, END)
             self.mail.delete(0, END)
             self.password.delete(0, END)
@@ -125,9 +121,10 @@ class Login:
 
     # Inicio de sesión
     def iniciar_sesion (self, userverify, passwordverify):
-        
+       
         cursor = cnx.cursor(buffered=True)
-        
+       
+       
         query = "SELECT contrasena, iduser FROM usuarios WHERE nombreuser = %s and contrasena = %s "
         parametros = (userverify.get(), passwordverify.get())
         cursor.execute(query, parametros)
@@ -139,21 +136,23 @@ class Login:
             messagebox.showinfo(message="Inicio de sesión correcta")
             iduser = resultado[0][1]
             self.wind.destroy()
-            Product()
+            Product(iduser)
 
-        
-        
+       
+       
 
 
 
        
-    
+   
 
 class Product:
-    def __init__(self):
-        # Arranca 
+    def __init__(self, iduser):
+        # Arranca
         self.wind = Tk()
-        self.wind.title('Control de productos')
+        self.wind.title('Control de productos:')
+       
+       
 
         # Contenedor
         frame = LabelFrame(self.wind, text = 'Añadir producto')
@@ -176,26 +175,14 @@ class Product:
         self.stock.grid(row = 3, column = 1)
 
         # Boton añadir producto
-        ttk.Button(frame, text = 'Agregar producto', command = self.anadir_producto).grid(row = 5, columnspan = 2, sticky = W + E, pady = 5)
+        ttk.Button(frame, text = 'Agregar producto', command = lambda: self.anadir_producto(iduser)).grid(row = 5, columnspan = 2, sticky = W + E, pady = 5)
 
         # Mensajes de salida
         self.message = Label(text = '', fg = 'green')
         self.message.grid(row = 4, column = 0, columnspan = 2, sticky = W + E)
 
-        # Tabla
-        self.tree = ttk.Treeview(height = 8, columns = ('#0', '#1', '#2'))
-        self.tree.grid(row = 3, column = 0, columnspan = 2)
-        self.tree.heading('#0', text = 'Id', anchor = CENTER)
-        self.tree.heading('#1', text = 'Nombre', anchor = CENTER)
-        self.tree.heading('#2', text = 'Precio', anchor = CENTER)
-        self.tree.heading('#3', text = 'Stock', anchor = CENTER)
-
-        # Botones
-        ttk.Button(text = 'ELIMINAR', command = self.eliminar_producto).grid(row = 5, column = 0, sticky = W + E)
-        ttk.Button(text = 'EDITAR', command = self.editar_producto).grid(row = 5, column = 1, sticky = W + E)
-
         # Llenar filas
-        self.obtener_productos()
+        self.obtener_productos(iduser)
 
     # Funcion que ejecuta consultas
     def consultar(self, query, parametros = ()):
@@ -209,15 +196,30 @@ class Product:
         return query
 
     # Obtener productos de la base de datos
-    def obtener_productos(self):
+    def obtener_productos(self, iduser):
+        
+         # Tabla
+        self.tree = ttk.Treeview(height = 8, columns = ('#0', '#1', '#2'))
+        self.tree.grid(row = 3, column = 0, columnspan = 2)
+        self.tree.heading('#0', text = 'Id', anchor = CENTER)
+        self.tree.heading('#1', text = 'Nombre', anchor = CENTER)
+        self.tree.heading('#2', text = 'Precio', anchor = CENTER)
+        self.tree.heading('#3', text = 'Stock', anchor = CENTER)
+
+         # Botones
+        ttk.Button(text = 'ELIMINAR', command = lambda: self.eliminar_producto(iduser)).grid(row = 5, column = 0, sticky = W + E)
+        ttk.Button(text = 'EDITAR', command = lambda: self.editar_producto(iduser)).grid(row = 5, column = 1, sticky = W + E)
+
+        
         # limpiar tabla
         records = self.tree.get_children()
         cursor = cnx.cursor(buffered=True)
         for element in records:
             self.tree.delete(element)
         # obtener data
-        query = "SELECT id,nombre,precio,stock FROM productos ORDER BY id DESC"
-        cursor.execute(query)
+        query = "SELECT productos.id, productos.nombre, productos.precio, productos.stock FROM productos LEFT JOIN usuarios on usuarios.iduser = productos.iduser WHERE usuarios.iduser = %s ORDER BY id DESC"
+        parametros = (iduser,)
+        cursor.execute(query, parametros)
         db_rows = cursor.fetchall()
         # llenando data
         if db_rows != None:
@@ -228,20 +230,18 @@ class Product:
     def validacion(self):
         return len(self.name.get()) != 0 and len(self.price.get()) != 0
 
-    def anadir_producto(self):
+    def anadir_producto(self, iduser):
         if self.validacion():
-            query = "INSERT INTO productos (id,nombre,precio,stock) VALUES (NULL,%s,%s,%s)"
-            parametros =  (self.name.get(), self.price.get(), self.stock.get())
+            query = "INSERT INTO productos (id,nombre,precio,stock, iduser) VALUES (NULL,%s,%s,%s, %s)"
+            parametros =  (self.name.get(), self.price.get(), self.stock.get(), iduser)
             self.consultar(query, parametros)
             self.message['text'] = 'Producto {} añadido'.format(self.name.get())
             self.name.delete(0, END)
             self.price.delete(0, END)
             self.stock.delete(0, END)
-        else:
-            self.message['text'] = 'Se requiere un precio, nombre y stock'
-        self.obtener_productos()
+        self.obtener_productos(iduser)
 
-    def eliminar_producto(self):
+    def eliminar_producto(self, iduser):
         self.message['text'] = ''
         if self.tree.item(self.tree.selection()) == None:
                 self.message['text'] = 'Por favor elija un producto'
@@ -252,9 +252,9 @@ class Product:
             cursor = cnx.cursor(buffered=True)
             cursor.execute(query)
             cnx.commit()
-            self.obtener_productos()
+            self.obtener_productos(iduser)
 
-    def editar_producto(self):
+    def editar_producto(self, iduser):
         self.message['text'] = ''
         try:
             self.tree.item(self.tree.selection())['values'][0]
@@ -273,7 +273,7 @@ class Product:
         new_name = Entry(self.edit_wind)
         new_name.grid(row = 1, column = 2)
 
-        # precio viejo 
+        # precio viejo
         Label(self.edit_wind, text = 'Precio antigüo:').grid(row = 2, column = 1)
         Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = old_price), state = 'readonly').grid(row = 2, column = 2)
         # precio nuevo
@@ -281,16 +281,16 @@ class Product:
         new_price= Entry(self.edit_wind)
         new_price.grid(row = 3, column = 2)
 
-        Button(self.edit_wind, text = 'Actualizar', command = lambda: self.editar_productos(new_name.get(), name, new_price.get(), old_price)).grid(row = 4, column = 2, sticky = W)
+        Button(self.edit_wind, text = 'Actualizar', command = lambda: self.editarproductos(new_name.get(), name, new_price.get(), old_price, iduser)).grid(row = 4, column = 2, sticky = W)
         self.edit_wind.mainloop()
 
-    def editar_productos(self, new_name, name, new_price, old_price):
+    def editarproductos(self, new_name, name, new_price, old_price, iduser):
         query = """UPDATE productos SET nombre = %s, precio = %s WHERE nombre = %s AND precio = %s"""
         parametros = (new_name, new_price, name, old_price)
         self.consultar(query, parametros)
         self.edit_wind.destroy()
         self.message['text'] = 'Producto {} actualizado'.format(name)
-        self.obtener_productos()
+        self.obtener_productos(iduser)
 
 if __name__ == '__main__':
     window = Tk()
